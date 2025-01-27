f7bb0874f7f7c0969090f0cb6e09b82d9279c2f4
Vadim <zankov.vadik@yandex.ru>
2025-01-27 11:46:23 +0500

    add settings vue-tg

--- src/main.js
+++ src/main.js
@@ -5,11 +5,13 @@ import i18n from "./i18n";
 import router from "./router/router";
 import "gitart-vue-dialog/dist/style.css";
 import { GDialog } from "gitart-vue-dialog";
+import { VueTelegramPlugin } from "vue-tg";
 
 axios.defaults.baseURL = "http://51.20.115.187:5050/";
 // axios.defaults.baseURL = "http://213.108.200.197:5050/";
 createApp(App)
   .use(router)
+  .use(VueTelegramPlugin)
   .component("GDialog", GDialog)
   .use(i18n)
   .mount("#app");
